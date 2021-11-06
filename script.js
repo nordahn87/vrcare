ff96b8fd3bc03def2d8ebdf5f3796543afc08c16
nordahn87 <charleynordahn87@gmail.com>
2021-11-06 11:05:55 +0100

    Added functions in js

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-window.addEventListener('scroll', function () {
-    let nav = document.querySelector('#nav');
-    let windowPosition = window.scrollY > 0;
-    nav.classList.toggle('scrolling-active', windowPosition);
+// Add class to navigation
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('#nav');
+    const windowPosition = window.scrollY;
+    if (windowPosition >= 0) {
+        nav.classList.toggle('scrolling-active', windowPosition);
+    }
 })
 
-const toogleMobileNav = () => {
-    let mobileNav = document.querySelector('.nav-mobile')
+const mobileNav = document.querySelector('.nav-mobile');
+
+const toggleMobileNav = () => {
     mobileNav.classList.toggle('display')
-  }
+}
+
+window.addEventListener('resize', () => {
+    const windowInnerWidth = window.innerWidth;
+    if (windowInnerWidth > 1050) {
+        mobileNav.classList.remove('display');
+    }
+})
+
+let backgroundVideo = document.querySelector('#bgvid');
+let a = 0;
+
+backgroundVideo.onended = () => {
+    a = a + 1;
+    if (a != 3) {
+        backgroundVideo.play();
+    }
+};
+
