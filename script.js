41259c77513ec62b1991d260b4abdaa83b6c23a9
nordahn87 <charleynordahn87@gmail.com>
2021-11-12 18:17:52 +0100

    Fix

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ window.addEventListener('scroll', () => {
     const windowPosition = window.scrollY;
     if (windowPosition >= 0) {
         nav.classList.toggle('scrolling-active', windowPosition);
-    }
+    } 
 })
+
+let parrent = document.querySelector('#nav-mobile-links');
+parrent.addEventListener("click", targetNavMobile);
+
+function targetNavMobile(e) {
+    if (e.target !== e.currentTarget) {
+        mobileNav.classList.remove('display');
+        burgerBtn.classList.remove('open');
+    }
+    e.stopPropagation();
+}
 
 const mobileNav = document.querySelector('.nav-mobile');
 
@@ -13,19 +24,23 @@ const toggleMobileNav = () => {
     mobileNav.classList.toggle('display')
 }
 
+const burgerBtn = document.querySelector('.burger-btn-container');
+let menuOpen = false;
+burgerBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        burgerBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        burgerBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
 window.addEventListener('resize', () => {
     const windowInnerWidth = window.innerWidth;
     if (windowInnerWidth > 1050) {
         mobileNav.classList.remove('display');
+        burgerBtn.classList.remove('open');
+        menuOpen = false;
     }
 })
-
-// let backgroundVideo = document.querySelector('#bgvid');
-// let a = 0;
-
-// backgroundVideo.onended = () => {
-//     a = a + 1;
-//     if (a != 3) {
-//         backgroundVideo.play();
-//     }
-// };
